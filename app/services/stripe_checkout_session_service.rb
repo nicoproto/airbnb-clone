b7644cf2815a41edfee33c3c05db461b56ff2d27
Nico Proto <nicolasproto@gmail.com>
2020-03-03 18:23:16 +1100

    test

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -2,6 +2,6 @@ class StripeCheckoutSessionService
   def call(event)
     booking = Booking.find_by(checkout_session_id: event.data.object.id)
     puts "#{event.data.object.id}"
-    booking.update(state: 'paid')
+    booking.update(state: 'Paid')
   end
 end
